4e3cf1875df1de336f9c578773e68418d60a3ff4
Carl Garner <carl.garner@anoto.com>
2014-06-26 21:05:44 +0100

    Rewrite rules Output errors in correct order Provide an application list, grouped by organisation

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
 
+#header('Content-type: text/plain');
+#print_r($_SERVER);
+#print_r(explode('/', ltrim($_SERVER['PATH_INFO'], '/')));
+#exit();
+
 header('Content-type: text/xml');
 
 require_once '/opt/dpp-registry/src/entities/classes.php';
+require_once 'helpers/applications.php';
 
 $user	= null;
 $xml	= simplexml_load_string('<tablet></tablet>');
@@ -10,20 +16,30 @@ $xml	= simplexml_load_string('<tablet></tablet>');
 $whitelist = array('localhost', '127.0.0.1', '::1');
 if(!in_array($_SERVER['HTTP_HOST'], $whitelist))
 {
-	if(!isset($_SERVER['HTTPS']) || strcasecmp($_SERVER['HTTPS'],"on") != 0)
+	if(!isset($_SERVER['HTTPS']) || strcasecmp($_SERVER['HTTPS'], 'on') != 0)
 	{
 		$xml->addChild('error', 'You must use SSL encryption to access this site');
+		exit($xml->asXML());
 	}
 
 	if(!isset($_SERVER['PHP_AUTH_USER']))
 	{
 		$xml->addChild('error', 'You must be authenticated to access this site');
+		exit($xml->asXML());
 	}
-
-	echo $xml->asXML();
-	exit;
 }
 
 $user = UserRepository::getUserHavingName($_SERVER['PHP_AUTH_USER']);
+
+$path = explode('/', ltrim($_SERVER['PATH_INFO'], '/'));
+
+switch($path[0])
+{
+	case 'applist':
+		getAppList($xml, $_SERVER['PHP_AUTH_USER']);
+		break;
+	default:
+		$xml->addChild('error', 'No recognised action set');
+}
 
 echo $xml->asXML();
